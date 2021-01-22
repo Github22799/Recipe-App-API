2110816f1d4df89a6805740c1cccf34c72fa9a94
Github22799 <31520065+Github22799@users.noreply.github.com>
2021-01-22 12:38:26 +0200

    initial code for the user app + some tests

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,61 @@
+from django.urls import reverse
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+CREATE_USER_URL = reverse('user:create')
+
+VALID_PAYLOAD_1 = {
+    'email': 'hello@hello.com',
+    'password': 'pass123',
+    'name': 'Hello 1'
+}
+
+SHORT_PASSWORD_PAYLOAD = {
+    'email': 'hello@hello.com',
+    'password': 'abc',
+    'name': 'Hello 1'
+}
+
+
+def get_user_manager():
+    return get_user_model().objects
+
+
+def get_user(**kwargs):
+    return get_user_manager().filter(**kwargs)
+
+
+def create_user(**kwargs):
+    return get_user_manager().create_user(**kwargs)
+
+
+class UnauthorizedUserAPITest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_valid_user_create(self):
+        response = self.client.post(**VALID_PAYLOAD_1)
+
+        user = get_user(**response.data)
+        password = VALID_PAYLOAD_1['password']
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(user.check_password(password))
+        self.assertNotIn(password, user.data)
+
+    def test_duplicate_user_create(self):
+        create_user(**VALID_PAYLOAD_1)
+        response = self.client.post(**VALID_PAYLOAD_1)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_short_password_user_create(self):
+        response = self.client.post(**SHORT_PASSWORD_PAYLOAD)
+
+        user_exists = get_user(**response.data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertFalse(user_exists)
